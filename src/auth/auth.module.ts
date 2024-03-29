59461efcfc27d42dfd12e27f61d7e257847ed366
Javier Villarreal <villarrealjavier401@gmail.com>
2024-03-29 16:50:17 -0500

    eliminación del controlador de auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -28,7 +27,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }),
  
   ],
-  controllers: [AuthController],
   providers: [AuthService],
   exports: [ AuthService]
 })
